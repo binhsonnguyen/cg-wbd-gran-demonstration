862432dae562c3718ef2b43819df3f08d275c201
edward <son@binhsonnguyen.com>
2019-07-12 09:44:34 +0700

    Let customer servlet resposebility on get request.
    Define doGet method for CustomerServlet.

--- src/main/java/cg/wbd/grandemonstration/servlet/CustomerServlet.java
+++ src/main/java/cg/wbd/grandemonstration/servlet/CustomerServlet.java
@@ -1,6 +1,13 @@
 package cg.wbd.grandemonstration.servlet;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getOutputStream().println("There are an unknown customer(s) in list.");
+    }
 }
