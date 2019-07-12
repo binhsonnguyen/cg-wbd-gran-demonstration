30ee4224d72199581c483a4316a86e15c81a0029
edward <son@binhsonnguyen.com>
2019-07-12 22:48:18 +0700

    Iteration on list customer and add customer information to table.

--- src/main/java/cg/wbd/grandemonstration/servlet/CustomerServlet.java
+++ src/main/java/cg/wbd/grandemonstration/servlet/CustomerServlet.java
@@ -1,5 +1,6 @@
 package cg.wbd.grandemonstration.servlet;
 
+import cg.wbd.grandemonstration.model.Customer;
 import cg.wbd.grandemonstration.service.CustomerService;
 import cg.wbd.grandemonstration.service.impl.SimpleCustomerServiceImpl;
 
@@ -7,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class CustomerServlet extends HttpServlet {
     private static final CustomerService CUSTOMER_SERVICE = new SimpleCustomerServiceImpl();
@@ -26,7 +28,23 @@ public class CustomerServlet extends HttpServlet {
                 .append("    <th>Address</th>")
                 .append("  </tr>")
                 .append("  </thead>")
+                .append("  <tbody>");
+
+        List<Customer> customers = CUSTOMER_SERVICE.findAll();
+        for (Customer c : customers) {
+            viewBuilder
+                    .append("  <tr>")
+                    .append(String.format("    <td>%d</td>", c.getId()))
+                    .append(String.format("    <td>%s</td>", c.getName()))
+                    .append(String.format("    <td>%s</td>", c.getEmail()))
+                    .append(String.format("    <td>%s</td>", c.getAddress()))
+                    .append("  </tr>");
+        }
+
+        viewBuilder
+                .append("  </tbody>")
                 .append("</table>");
+
         String view = viewBuilder.toString();
         resp.getOutputStream().println(view);
         resp.setContentType("text/html");
