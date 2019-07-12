fbeea13b445dd25dda71efa6210577064deadb27
edward <son@binhsonnguyen.com>
2019-07-12 22:21:51 +0700

    Use customer service to get count of customer and merge into view.

--- src/main/java/cg/wbd/grandemonstration/servlet/CustomerServlet.java
+++ src/main/java/cg/wbd/grandemonstration/servlet/CustomerServlet.java
@@ -1,13 +1,21 @@
 package cg.wbd.grandemonstration.servlet;
 
+import cg.wbd.grandemonstration.service.CustomerService;
+import cg.wbd.grandemonstration.service.impl.SimpleCustomerServiceImpl;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CustomerServlet extends HttpServlet {
+    private static final CustomerService CUSTOMER_SERVICE = new SimpleCustomerServiceImpl();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.getOutputStream().println("There are an unknown customer(s) in list.");
+        String template = "There are an %s customer(s) in list.";
+        long customerCount = CUSTOMER_SERVICE.count();
+        String view = String.format(template, customerCount);
+        resp.getOutputStream().println(view);
     }
 }
