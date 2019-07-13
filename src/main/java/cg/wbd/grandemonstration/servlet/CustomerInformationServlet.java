31a455bdac8992e1932be4922fd1c0d925d93cbf
edward <son@binhsonnguyen.com>
2019-07-13 21:42:46 +0700

    Show customer information in a fieldset.

--- src/main/java/cg/wbd/grandemonstration/servlet/CustomerInformationServlet.java
+++ src/main/java/cg/wbd/grandemonstration/servlet/CustomerInformationServlet.java
@@ -16,6 +16,37 @@ public class CustomerInformationServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         Long id = Long.valueOf(req.getParameter("id"));
         Customer customer = customerService.findOne(id);
-        resp.getOutputStream().println("Hello world! " + customer);
+        StringBuilder viewBuilder = new StringBuilder()
+                .append("<fieldset>")
+                .append("  <legend>Customer Information</legend>")
+                .append("  <table>")
+                .append("    <tr>")
+                .append("      <td>Id</td>")
+                .append("      <td>").append(String.format(
+                        "        <input type='text' value='%d'>", customer.getId()))
+                .append("      </td>")
+                .append("    </tr>")
+                .append("    <tr>")
+                .append("      <td>Name</td>")
+                .append("      <td>").append(String.format(
+                        "        <input type='text' value='%s'>", customer.getName()))
+                .append("      </td>")
+                .append("    </tr>")
+                .append("    <tr>")
+                .append("      <td>Email</td>")
+                .append("      <td>").append(String.format(
+                        "        <input type='text' value='%s'>", customer.getEmail()))
+                .append("      </td>")
+                .append("    </tr>")
+                .append("    <tr>")
+                .append("      <td>Address</td>")
+                .append("      <td>").append(String.format(
+                        "        <input type='text' value='%s'>", customer.getAddress()))
+                .append("      </td>")
+                .append("    </tr>")
+                .append("  </table>")
+                .append("</fieldset>");
+        resp.getOutputStream().println(viewBuilder.toString());
+        resp.setContentType("text/html");
     }
 }
