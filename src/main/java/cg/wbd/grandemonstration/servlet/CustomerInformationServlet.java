9db0f9eddc393ad8fd7f11e757e62558e2aa62dc
edward <son@binhsonnguyen.com>
2019-07-13 08:51:24 +0700

    Use comtomer service to get customer model.

--- src/main/java/cg/wbd/grandemonstration/servlet/CustomerInformationServlet.java
+++ src/main/java/cg/wbd/grandemonstration/servlet/CustomerInformationServlet.java
@@ -1,14 +1,21 @@
 package cg.wbd.grandemonstration.servlet;
 
+import cg.wbd.grandemonstration.model.Customer;
+import cg.wbd.grandemonstration.service.CustomerService;
+import cg.wbd.grandemonstration.service.impl.SimpleCustomerServiceImpl;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CustomerInformationServlet extends HttpServlet {
+    private static final CustomerService CUSTOMER_SERVICE = new SimpleCustomerServiceImpl();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         Long id = Long.valueOf(req.getParameter("id"));
-        resp.getOutputStream().println("Hello world! " + id);
+        Customer customer = CUSTOMER_SERVICE.findOne(id);
+        resp.getOutputStream().println("Hello world! " + customer);
     }
 }
