db21ae500659a2ddcffe99d78a71022fceeafe94
edward <son@binhsonnguyen.com>
2019-07-13 08:48:09 +0700

    Catch customer information get requests.

--- src/main/java/cg/wbd/grandemonstration/servlet/CustomerInformationServlet.java
+++ src/main/java/cg/wbd/grandemonstration/servlet/CustomerInformationServlet.java
@@ -1,6 +1,13 @@
 package cg.wbd.grandemonstration.servlet;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class CustomerInformationServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getOutputStream().println("Hello world!");
+    }
 }
