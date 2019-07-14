05df53694e7b1beaca965ab174c42331ef19aecb
edward <son@binhsonnguyen.com>
2019-07-14 16:24:05 +0700

    Use annotation base mechanic to make customer controller works.

--- src/main/java/cg/wbd/grandemonstration/controller/CustomerController.java
+++ src/main/java/cg/wbd/grandemonstration/controller/CustomerController.java
@@ -1,7 +1,9 @@
 package cg.wbd.grandemonstration.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class CustomerController {
     @GetMapping("/customers")
     public String showList() {
