0a53862d3bc9c477618d74b35c33065f8155a5ec
edward <son@binhsonnguyen.com>
2019-07-15 00:21:13 +0700

    Annotate that customer repository implement with orm is transactional.

--- src/main/java/cg/wbd/grandemonstration/repository/impl/CustomerRepositoryImpl.java
+++ src/main/java/cg/wbd/grandemonstration/repository/impl/CustomerRepositoryImpl.java
@@ -7,8 +7,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
 public class CustomerRepositoryImpl implements CustomerRepository {
     @PersistenceContext
     private EntityManager em;
