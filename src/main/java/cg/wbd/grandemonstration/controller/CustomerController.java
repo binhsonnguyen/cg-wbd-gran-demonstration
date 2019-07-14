6549d7083330e751b07516f86cfd83af2471125e
edward <son@binhsonnguyen.com>
2019-07-14 20:55:35 +0700

    Use Model class to store model instead of request attribute mechanic.

--- src/main/java/cg/wbd/grandemonstration/controller/CustomerController.java
+++ src/main/java/cg/wbd/grandemonstration/controller/CustomerController.java
@@ -4,9 +4,9 @@ import cg.wbd.grandemonstration.model.Customer;
 import cg.wbd.grandemonstration.service.CustomerService;
 import cg.wbd.grandemonstration.service.CustomerServiceFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -14,9 +14,9 @@ public class CustomerController {
     private CustomerService customerService = CustomerServiceFactory.getInstance();
 
     @GetMapping("/customers")
-    public String showList(HttpServletRequest request) {
+    public String showList(Model model) {
         List<Customer> customers = customerService.findAll();
-        request.setAttribute("customers", customers);
+        model.addAttribute("customers", customers);
         return "customers/list.jsp";
     }
 }
