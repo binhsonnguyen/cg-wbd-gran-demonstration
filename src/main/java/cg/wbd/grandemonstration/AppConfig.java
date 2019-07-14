de3823680f69721fb7e08eb6e9dad30f90a72405
edward <son@binhsonnguyen.com>
2019-07-14 22:19:58 +0700

    Use full java based configurations.

--- src/main/java/cg/wbd/grandemonstration/AppConfig.java
+++ src/main/java/cg/wbd/grandemonstration/AppConfig.java
@@ -3,12 +3,17 @@ package cg.wbd.grandemonstration;
 import cg.wbd.grandemonstration.service.CustomerService;
 import cg.wbd.grandemonstration.service.impl.SimpleCustomerServiceImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class AppConfig {
+@EnableWebMvc
+@ComponentScan("cg.wbd.grandemonstration")
+public class AppConfig extends WebMvcConfigurerAdapter {
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
