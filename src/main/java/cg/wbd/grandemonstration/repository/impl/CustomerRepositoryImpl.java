b58c6b596f25647704b207212335579a1feb4f39
edward <son@binhsonnguyen.com>
2019-07-15 00:13:12 +0700

    Define a dummy implement of customer repository interface.

---
+++ src/main/java/cg/wbd/grandemonstration/repository/impl/CustomerRepositoryImpl.java
@@ -0,0 +1,24 @@
+package cg.wbd.grandemonstration.repository.impl;
+
+import cg.wbd.grandemonstration.model.Customer;
+import cg.wbd.grandemonstration.repository.CustomerRepository;
+
+import java.util.List;
+
+public class CustomerRepositoryImpl implements CustomerRepository {
+    public List<Customer> findAll() {
+        return null;
+    }
+
+    public Customer findById(Long id) {
+        return null;
+    }
+
+    public void save(Customer model) {
+
+    }
+
+    public void remove(Long id) {
+
+    }
+}
