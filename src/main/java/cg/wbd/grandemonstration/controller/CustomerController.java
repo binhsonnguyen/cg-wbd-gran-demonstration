45215d453cc0bdb1fbfc5be35282468ffde574d6
edward <son@binhsonnguyen.com>
2019-07-14 16:40:40 +0700

    Use ModelAndView api instead of inject model object into controller by method parametter way.

--- src/main/java/cg/wbd/grandemonstration/controller/CustomerController.java
+++ src/main/java/cg/wbd/grandemonstration/controller/CustomerController.java
@@ -4,8 +4,8 @@ import cg.wbd.grandemonstration.model.Customer;
 import cg.wbd.grandemonstration.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -15,9 +15,10 @@ public class CustomerController {
     private CustomerService customerService;
 
     @GetMapping("/customers")
-    public String showList(Model model) {
+    public ModelAndView showList() {
+        ModelAndView modelAndView = new ModelAndView("customers/list.jsp");
         List<Customer> customers = customerService.findAll();
-        model.addAttribute("customers", customers);
-        return "customers/list.jsp";
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
     }
 }
