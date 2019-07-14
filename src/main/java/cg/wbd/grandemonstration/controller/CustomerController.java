558927ae59f5b5e0b73abcc481233d5a0af18196
edward <son@binhsonnguyen.com>
2019-07-14 21:01:22 +0700

    Create handle method for customer information requests.

--- src/main/java/cg/wbd/grandemonstration/controller/CustomerController.java
+++ src/main/java/cg/wbd/grandemonstration/controller/CustomerController.java
@@ -5,6 +5,7 @@ import cg.wbd.grandemonstration.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -19,6 +20,12 @@ public class CustomerController {
         ModelAndView modelAndView = new ModelAndView("customers/list");
         List<Customer> customers = customerService.findAll();
         modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+
+    @GetMapping("/customers/{id}")
+    public ModelAndView showInformation(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("customers/info");
         return modelAndView;
     }
 }
