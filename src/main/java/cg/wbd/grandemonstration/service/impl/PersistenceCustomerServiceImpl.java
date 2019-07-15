ebaa6a653228804c2bacf0a90f656446024d593a
edward <son@binhsonnguyen.com>
2019-07-15 11:02:12 +0700

    Inject customer repository into customer service implement.

--- src/main/java/cg/wbd/grandemonstration/service/impl/PersistenceCustomerServiceImpl.java
+++ src/main/java/cg/wbd/grandemonstration/service/impl/PersistenceCustomerServiceImpl.java
@@ -1,11 +1,16 @@
 package cg.wbd.grandemonstration.service.impl;
 
 import cg.wbd.grandemonstration.model.Customer;
+import cg.wbd.grandemonstration.repository.CustomerRepository;
 import cg.wbd.grandemonstration.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class PersistenceCustomerServiceImpl implements CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
     @Override
     public List<Customer> findAll() {
         return null;
