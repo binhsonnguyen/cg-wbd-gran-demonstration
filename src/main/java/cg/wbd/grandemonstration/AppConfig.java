36803100586bd6f70c97aa8e6ce218f0446b00ca
edward <son@binhsonnguyen.com>
2019-07-15 11:51:12 +0700

    Add connect information into datasource.

--- src/main/java/cg/wbd/grandemonstration/AppConfig.java
+++ src/main/java/cg/wbd/grandemonstration/AppConfig.java
@@ -77,6 +77,9 @@ public class AppConfig extends WebMvcConfigurerAdapter implements ApplicationCon
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/cms");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123456");
         return dataSource;
     }
 
