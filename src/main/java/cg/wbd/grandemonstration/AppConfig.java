457f6b72cdfdce377e77306b748941b23f46b433
edward <son@binhsonnguyen.com>
2019-07-16 15:56:40 +0700

    Get config of view resolver's prefix and suffix from properties file.

--- src/main/java/cg/wbd/grandemonstration/AppConfig.java
+++ src/main/java/cg/wbd/grandemonstration/AppConfig.java
@@ -2,6 +2,7 @@ package cg.wbd.grandemonstration;
 
 import cg.wbd.grandemonstration.service.CustomerService;
 import cg.wbd.grandemonstration.service.impl.SimpleCustomerServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +17,13 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan("cg.wbd.grandemonstration")
 @PropertySource("classpath:application.properties")
 public class AppConfig extends WebMvcConfigurerAdapter {
+    @Value( "${template.location.prefix}" ) private String templateLocationPrefix;
+    @Value( "${template.location.suffix}" ) private String templateLocationSuffix;
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/templates/");
-        viewResolver.setSuffix(".jsp");
+        viewResolver.setPrefix(templateLocationPrefix);
+        viewResolver.setSuffix(templateLocationSuffix);
         return viewResolver;
     }
 
