e2602c597feb122a0c3a7202f1e3f98867a7dc02
edward <son@binhsonnguyen.com>
2019-07-18 08:53:23 +0700

    Try to throw error on fields error instead of update customers.

--- src/main/java/cg/wbd/grandemonstration/controller/CustomerController.java
+++ src/main/java/cg/wbd/grandemonstration/controller/CustomerController.java
@@ -5,6 +5,7 @@ import cg.wbd.grandemonstration.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,7 +37,10 @@ public class CustomerController {
     }
 
     @PostMapping
-    public String updateCustomer(Customer customer, BindingResult bindingResult) {
+    public String updateCustomer(Customer customer, BindingResult bindingResult) throws ServletRequestBindingException {
+        if (bindingResult.hasFieldErrors()) {
+            throw new ServletRequestBindingException("There are field errors!");
+        }
         customerService.save(customer);
         return "redirect:/customers";
     }
