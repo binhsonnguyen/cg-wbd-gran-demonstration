79490f390153aae6c4150dad6a2d2f0ebb9d2453
edward <son@binhsonnguyen.com>
2019-07-21 09:53:36 +0700

    Accept user credential from login form.

--- src/main/java/cg/wbd/grandemonstration/controller/UserController.java
+++ src/main/java/cg/wbd/grandemonstration/controller/UserController.java
@@ -1,7 +1,9 @@
 package cg.wbd.grandemonstration.controller;
 
+import cg.wbd.grandemonstration.model.UserCredential;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -9,6 +11,12 @@ public class UserController {
     @GetMapping("/login")
     public ModelAndView showLoginForm() {
         ModelAndView modelAndView = new ModelAndView("login");
+        modelAndView.addObject("userCredential", new UserCredential());
         return modelAndView;
+    }
+
+    @PostMapping("/login")
+    public String login(UserCredential userCredential) {
+        return "redirect:/customers";
     }
 }
