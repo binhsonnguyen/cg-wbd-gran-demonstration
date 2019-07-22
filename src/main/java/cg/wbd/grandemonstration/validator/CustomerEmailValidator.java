0196e8e03eb68c11a5d1422e8fed7f56ff295f7e
edward <son@binhsonnguyen.com>
2019-07-22 09:49:39 +0700

    Reject all customer's email because of no reason.

--- src/main/java/cg/wbd/grandemonstration/validator/CustomerEmailValidator.java
+++ src/main/java/cg/wbd/grandemonstration/validator/CustomerEmailValidator.java
@@ -1,15 +1,17 @@
 package cg.wbd.grandemonstration.validator;
 
+import cg.wbd.grandemonstration.model.Customer;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 public class CustomerEmailValidator implements Validator {
     @Override
     public boolean supports(Class<?> clazz) {
-        return false;
+        return Customer.class.isAssignableFrom(clazz);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
+        errors.rejectValue("email", null, "No reason!");
     }
 }
