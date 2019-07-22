61d6d67d96260ccfdfffd8817e77f7af3f3522db
edward <son@binhsonnguyen.com>
2019-07-22 10:59:42 +0700

    Let UniqueEmail annotation available at runtime.

--- src/main/java/cg/wbd/grandemonstration/validator/UniqueEmail.java
+++ src/main/java/cg/wbd/grandemonstration/validator/UniqueEmail.java
@@ -1,7 +1,10 @@
 package cg.wbd.grandemonstration.validator;
 
 import javax.validation.Constraint;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 @Constraint(validatedBy = UniqueEmailValidator.class)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface UniqueEmail {
 }
