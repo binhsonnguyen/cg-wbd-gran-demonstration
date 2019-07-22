bbd4ab7e75dd721de434c722747aa7e075a3f777
edward <son@binhsonnguyen.com>
2019-07-22 11:01:42 +0700

    Add params that needed by Constraint interface.

--- src/main/java/cg/wbd/grandemonstration/validator/UniqueEmail.java
+++ src/main/java/cg/wbd/grandemonstration/validator/UniqueEmail.java
@@ -1,10 +1,14 @@
 package cg.wbd.grandemonstration.validator;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Constraint(validatedBy = UniqueEmailValidator.class)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UniqueEmail {
+    String message() default "Email is exists!";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
