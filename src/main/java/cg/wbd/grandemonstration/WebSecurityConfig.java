0ee1d68cde70bececc80600870f432066046043b
edward <son@binhsonnguyen.com>
2019-07-24 10:18:52 +0700

    Permit all request except of customers endpoints.

--- src/main/java/cg/wbd/grandemonstration/WebSecurityConfig.java
+++ src/main/java/cg/wbd/grandemonstration/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").denyAll();
+        http.authorizeRequests().antMatchers("/customers/**").authenticated()
+                .and().authorizeRequests().antMatchers("/**").permitAll();
     }
 }
